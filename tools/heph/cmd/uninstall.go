04e2cebfeb38a94315b82a31f4a6b34dae6c5d93
Alex Eales <dev@alex-eales.com>
2021-10-21 23:12:26 +0100

    Implemented uninstall command

--- tools/heph/cmd/uninstall.go
+++ tools/heph/cmd/uninstall.go
@@ -1,6 +1,13 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/AlexEales/go-micro-tmpl/tools/heph/helm"
+	"github.com/spf13/cobra"
+)
 
 var uninstallCmd = &cobra.Command{
 	Use:   "uninstall",
@@ -8,6 +15,37 @@ var uninstallCmd = &cobra.Command{
 	Long:  `Uninstalls the projects microservices from the k8s cluster currently active in the k8s config`,
 	Args:  cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Uninstalling project")
+		if err := uninstall(); err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 	},
+}
+
+func uninstall() error {
+	helmClient, err := helm.NewClient()
+	if err != nil {
+		return err
+	}
+
+	charts, err := helmClient.ListCharts()
+	if err != nil {
+		return err
+	}
+
+	log.Infof("uninstalling helm charts: {%s}", strings.Join(charts, ", "))
+	for _, chart := range charts {
+		if err := helmClient.UninstallChart(chart); err != nil {
+			return err
+		}
+		log.Infof("uninstalled chart %s", chart)
+	}
+
+	log.Infoln("uninstalling all k8s resources")
+	if err := exec.Command("kubectl", "delete", "all", "--all").Run(); err != nil {
+		return err
+	}
+
+	log.Infoln("cleared local cluster")
+	return nil
 }
--- tools/heph/main.go
+++ tools/heph/main.go
@@ -1,83 +1,16 @@
 package main
 
 import (
-	"context"
-	"path/filepath"
-	"time"
-
 	"github.com/AlexEales/go-micro-tmpl/tools/heph/cmd"
-	"github.com/AlexEales/go-micro-tmpl/tools/heph/helm"
-	"github.com/AlexEales/go-micro-tmpl/tools/heph/k8s"
 	"github.com/sirupsen/logrus"
-	"k8s.io/client-go/util/homedir"
 )
 
 type Command func() error
 
 var (
-	commandMap = map[string]Command{
-		"install":   install,
-		"uninstall": uninstall,
-	}
 	// TODO: not a fan of the logrus formatting should make a common lib with a common format
 	log = logrus.New()
 )
-
-func install() error {
-	helmClient, err := helm.NewClient()
-	if err != nil {
-		return err
-	}
-
-	k8sClient, err := k8s.NewClient(filepath.Join(homedir.HomeDir(), ".kube", "config"))
-	if err != nil {
-		return err
-	}
-
-	if err := helmClient.AddRepositories(map[string]string{
-		"bitnami":   "https://charts.bitnami.com/bitnami",
-		"hashicorp": "https://helm.releases.hashicorp.com",
-	}); err != nil {
-		return err
-	}
-
-	if err := helmClient.InstallCharts(map[string]*helm.Chart{
-		"postgres": {
-			Name:          "bitnami/postgresql",
-			OverridesFile: "infra/postgres/override-values.yaml",
-		},
-	}); err != nil {
-		return err
-	}
-
-	if err := k8sClient.WaitForPodsToBeReady(context.TODO(), "default", []string{"postgres-primary-0", "postgres-read-0"}, time.Minute); err != nil {
-		return err
-	}
-
-	log.Infoln("Installed!")
-	return nil
-}
-
-func uninstall() error {
-	helmClient, err := helm.NewClient()
-	if err != nil {
-		return err
-	}
-
-	charts, err := helmClient.ListCharts()
-	if err != nil {
-		return err
-	}
-
-	for _, chart := range charts {
-		if err := helmClient.UninstallChart(chart); err != nil {
-			return err
-		}
-		log.Infof("uninstalled chart %s", chart)
-	}
-
-	return nil
-}
 
 func main() {
 	cmd.Execute()
