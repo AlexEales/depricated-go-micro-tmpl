5f1f58eac6e4bba34935f4338d9d1fa5a46f452a
Alex Eales <dev@alex-eales.com>
2021-10-21 23:04:46 +0100

    Implemented simple install command

--- tools/heph/cmd/deploy.go
+++ tools/heph/cmd/deploy.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var deployCmd = &cobra.Command{
 	Use:   "deploy [path to service]",
-	Short: "Depoys the specified service",
+	Short: "Deploys the specified service",
 	Long:  `Deploys the specified service to the k8s cluster currently active in the kube config.`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
--- tools/heph/cmd/install.go
+++ tools/heph/cmd/install.go
@@ -1,6 +1,18 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"math"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/AlexEales/go-micro-tmpl/tools/heph/helm"
+	"github.com/AlexEales/go-micro-tmpl/tools/heph/k8s"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/util/homedir"
+)
 
 var installCmd = &cobra.Command{
 	Use:   "install",
@@ -8,6 +20,52 @@ var installCmd = &cobra.Command{
 	Long:  `Installs the projects microservices and sets up the project on the k8s cluster currently active in the k8s config`,
 	Args:  cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Installing project")
+		if err := install(); err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 	},
+}
+
+func install() error {
+	startTime := time.Now()
+
+	helmClient, err := helm.NewClient()
+	if err != nil {
+		return err
+	}
+
+	k8sClient, err := k8s.NewClient(filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		return err
+	}
+
+	if err := helmClient.AddRepositories(map[string]string{
+		"bitnami":   "https://charts.bitnami.com/bitnami",
+		"hashicorp": "https://helm.releases.hashicorp.com",
+	}); err != nil {
+		return err
+	}
+
+	if err := helmClient.InstallCharts(map[string]*helm.Chart{
+		"postgres": {
+			Name:          "bitnami/postgresql",
+			OverridesFile: "infra/postgres/override-values.yaml",
+		},
+	}); err != nil {
+		return err
+	}
+
+	pods := []string{
+		"postgres-primary-0",
+		"postgres-read-0",
+	}
+	log.Infof("Waiting for pods to be ready: {%s}", strings.Join(pods, ", "))
+	if err := k8sClient.WaitForPodsToBeReady(context.TODO(), "default", pods, time.Minute); err != nil {
+		return err
+	}
+
+	elapsed := time.Since(startTime)
+	log.Infof("installed successfully in %.2gm%.2gs", math.Round(elapsed.Minutes()), math.Round(elapsed.Seconds()))
+	return nil
 }
